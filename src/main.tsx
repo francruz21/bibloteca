4cec1d8f07ddb773be4de3da9d608ec7d9d499e9
Fran Cruz <157864846+francruz21@users.noreply.github.com>
2025-03-20 03:34:39 +0000

    <Update>: se modifico la importacion de body

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.tsx'
 import Footer from './components/Footer.tsx'
-import Body from './components/Body.tsx'
 import NoticiasCarousel from './components/NoticiasCarousel.tsx'
 
 createRoot(document.getElementById('root')!).render(
